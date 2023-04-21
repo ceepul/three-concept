fc982b4b05ee324e3568c7d18800c9614a24e128
Russell Fenton <russell1837@gmail.com>
2023-04-21 22:05:39 +1000

    updated a bunch of files to try and get it work with vercel.. doubtful

--- server-dev.ts
+++ server-dev.ts
@@ -9,7 +9,11 @@ import {getLocaleFromRequest} from '~/lib/utils';
  * Export a fetch handler in module format.
  */
 export default {
-  async fetch(request, env, executionContext) {
+  async fetch(
+    request: Request,
+    env: Env,
+    executionContext: ExecutionContext,
+  ): Promise {
     try {
       /**
        * Open a cache instance in the worker and a custom session instance.
@@ -18,7 +22,7 @@ export default {
         throw new Error('SESSION_SECRET environment variable is not set');
       }
 
-      const waitUntil = (p) => executionContext.waitUntil(p);
+      const waitUntil = (p: Promise) => executionContext.waitUntil(p);
       const [cache, session] = await Promise.all([
         caches.open('hydrogen'),
         HydrogenSession.init(request, [env.SESSION_SECRET]),
@@ -36,7 +40,7 @@ export default {
         privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
         storeDomain: `https://${env.PUBLIC_STORE_DOMAIN}`,
         storefrontApiVersion: env.PUBLIC_STOREFRONT_API_VERSION || '2023-01',
-        storefrontId: env.PUBLIC_STOREFRONT_ID,
+        // storefrontId: env.PUBLIC_STOREFRONT_ID,
         requestGroupId: request.headers.get('request-id'),
       });
 
